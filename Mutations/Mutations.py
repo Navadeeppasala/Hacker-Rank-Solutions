777c13a364e970b5b6d918aaec43eed3d4c728a9
Navadeep Pasala <70601227+Navadeeppasala@users.noreply.github.com>
2021-02-17 15:15:21 +0530

    Add files via upload

---
+++ Mutations/Mutations.py
@@ -0,0 +1,20 @@
+"""
+Task 
+Read a given string, change the character at a given index and then print the modified string.
+
+Input Format 
+The first line contains a string, S. 
+The next line contains an integer i, denoting the index location and a character c separated by a space.
+
+Output Format 
+Using any of the methods explained above, replace the character at index i with character c.
+
+Sample Input
+abracadabra
+5 k
+
+Sample Output
+abrackdabra
+"""
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position+1:]
